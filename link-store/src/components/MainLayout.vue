6fee9e26affedc584e8e8db204fff0ceea7d9381
YuriiRG <yuriirg@tutanota.com>
2021-11-09 20:58:11 +0200

    Create some of action side-buttons

--- link-store/src/components/MainLayout.vue
+++ link-store/src/components/MainLayout.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="d-flex flex-row flex-grow-1">
-        <div class="bg-light border-end">
-            Actions<i class="bi bi-file-earmark-plus"></i>
+        <div class="bg-light border-end action-sidebar">
+            <div class="border action-icon-block" title="Add new link">
+                <i class="bi bi-file-earmark-plus action-icon"></i>
+            </div>
+            <div class="border action-icon-block" title="Add new folder">
+                <i class="bi bi-folder-plus action-icon"></i>
+            </div>
         </div>
         <main class="flex-grow-1">
             Main table
@@ -13,3 +18,18 @@ export default {
     name: 'MainLayout'
 }
 </script>
+<style scoped>
+.action-icon {
+    font-size: 3em;
+}
+.action-icon-block {
+    width: 4em;
+    height: 4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.action-sidebar {
+    width: 4em;
+}
+</style>
