83aab17a407a4a65f050864cf36b54c2b5c5352d
YuriiRG <yuriirg@tutanota.com>
2021-11-09 14:58:36 +0200

    Create flex sidebar layout

--- link-store/src/components/MainLayout.vue
+++ link-store/src/components/MainLayout.vue
@@ -1,5 +1,12 @@
 <template>
-    MainLayout
+    <div class="d-flex flex-row flex-grow-1">
+        <div class="bg-light border-end">
+            Actions
+        </div>
+        <main class="flex-grow-1">
+            Main table
+        </main>
+    </div>
 </template>
 <script>
 export default {
