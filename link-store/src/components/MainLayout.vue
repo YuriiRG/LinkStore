14cd32e6433113d88c31c808d8ffbe5836102780
YuriiRG <yuriirg@tutanota.com>
2021-11-10 02:02:38 +0200

    Expand actions sidebar

--- link-store/src/components/MainLayout.vue
+++ link-store/src/components/MainLayout.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="d-flex flex-row flex-grow-1">
         <div class="bg-light border-end action-sidebar">
-            <div class="border action-icon-block" title="Add new link">
+            <div class="border action-icon-block btn" title="Add new link" v-on:click="newLink()">
                 <i class="bi bi-file-earmark-plus action-icon"></i>
             </div>
-            <div class="border action-icon-block" title="Add new folder">
+            <div class="border action-icon-block btn" title="Add new folder" v-on:click="newFolder()">
                 <i class="bi bi-folder-plus action-icon"></i>
+            </div>
+            <div class="border action-icon-block btn" title="Remove link" v-on:click="removeLink()">
+                <i class="bi bi-file-earmark-minus action-icon"></i>
+            </div>
+            <div class="border action-icon-block btn" title="Remove folder" v-on:click="removeFolder()">
+                <i class="bi bi-folder-minus action-icon"></i>
             </div>
         </div>
         <main class="flex-grow-1">
@@ -15,7 +21,21 @@
 </template>
 <script>
 export default {
-    name: 'MainLayout'
+    name: 'MainLayout',
+    methods: {
+        newLink() {
+            alert("newLink");
+        },
+        newFolder() {
+            alert("newFolder");
+        },
+        removeLink() {
+            alert("removeLink");
+        },
+        removeFolder() {
+            alert("removeFolder");
+        }
+    }
 }
 </script>
 <style scoped>
