13590a28a74f123f8976eed3a1a11b2dd3d459ff
YuriiRG <yuriirg@tutanota.com>
2021-11-11 21:32:32 +0200

    Move app.css to assets folder

--- link-store/src/main.js
+++ link-store/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-import "../public/app.css"
+import "./assets/app.css"
 import "./assets/icons/bootstrap-icons.css"
 
 createApp(App).mount('#app')
