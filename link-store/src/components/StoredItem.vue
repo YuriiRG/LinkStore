d734d4c4eb2af07983428b6d6aa28dd0262d00c7
YuriiRG <yuriirg@tutanota.com>
2021-11-12 01:53:11 +0200

    Fix space near the icon of link/folder

--- link-store/src/components/StoredItem.vue
+++ link-store/src/components/StoredItem.vue
@@ -1,11 +1,11 @@
 <template>
     <tr>
-        <td><i class="bi bi-folder"></i> PubChem</td>
+        <td><i class="bi bi-folder me-1"></i>PubChem</td>
         <td>Chemical search engine</td>
         <td>11.11.1111</td>
     </tr>
     <tr>
-        <td><i class="bi bi-folder"></i> PubChem search engine</td>
+        <td><i class="bi bi-link me-1"></i>PubChem search engine</td>
         <td>search engine</td>
         <td>11.11.1111</td>
     </tr>
